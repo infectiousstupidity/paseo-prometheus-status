c2825cf046734a54076c6325c0aed42c781ce396
infectiousstupidity <sub@fakeout.no>
2026-09-06 06:23:04 +0200

    feat: register Prometheus config handlers

--- index.ts
+++ index.ts
@@ -1,10 +1,20 @@
 import type { PluginContext } from "@getpaseo/plugin";
 import { contributeGpuStatusPills, GpuStatusPanel } from "./status.client";
-import { getGpuStatus } from "./status.server";
-import { gpuStatusGet } from "./status.shared";
+import {
+  describeGpuStatusConfig,
+  getGpuStatus,
+  saveGpuStatusConfig,
+} from "./status.server";
+import {
+  gpuStatusConfigGet,
+  gpuStatusConfigSave,
+  gpuStatusGet,
+} from "./status.shared";
 
 export default function contribute(plugin: PluginContext) {
   plugin.handle(gpuStatusGet, getGpuStatus);
+  plugin.handle(gpuStatusConfigGet, () => describeGpuStatusConfig());
+  plugin.handle(gpuStatusConfigSave, saveGpuStatusConfig);
   plugin.addWorkspacePanel({
     id: "gpu-status",
     title: "GPU status",
